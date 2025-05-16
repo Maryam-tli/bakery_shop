59c9bd0563dd3937bc01f245dff3257236762919
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-15 19:28:54 -0700

    defined BookingAdmin

--- app_bakery/admin.py
+++ app_bakery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app_bakery.models import *
 
 # Register your models here.
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email', 'guests', 'date', 'time']
+    search_fields = ['name', 'phone', 'email']
+    list_filter = ['date',]
+    date_hierarchy = 'date'
