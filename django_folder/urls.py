9876e3ef4fa95381caaa529fd85d21f5c80ac2da
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-17 08:21:17 -0700

    defined a new path for include all the path that I want to define in app_blug urls

--- django_folder/urls.py
+++ django_folder/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_bakery.urls')),
+    path('blog/', include('app_blog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
