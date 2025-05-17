3c536db2adfcc580d71654367289eac7eeddab53
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-17 08:24:15 -0700

    fixed the home_view function to connect booking form in the html page with database

--- app_bakery/views.py
+++ app_bakery/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from app_bakery.forms import *
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+        else:
+            print("PHONE VALUE:", request.POST.get('phone'))
+            print(form.errors)
+            return HttpResponse('Your Information Is Not Valid')
+    form = BookingForm()
+    return render(request, 'home.html', {'booking':form})
 
 def about_view(request):
     return render(request, 'about-us.html')
