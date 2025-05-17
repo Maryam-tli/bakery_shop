4b434c3e9fe730f800d2553d5a01c67549107d78
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-17 08:14:41 -0700

    made Post model

---
+++ app_blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+from django.utils import timezone
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ManyToManyField(Category, blank=True)
+    image = models.ImageField(upload_to='posts/', blank=True, null=True, default='posts/default.jpg')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    status = models.BooleanField(default=False)
+    counted_views = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        if self.status and not self.published_date:
+            self.published_date = timezone.now()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_date']
