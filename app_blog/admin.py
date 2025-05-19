653a28d3fe0d0f508d3324a57ebea79ce50dacb9
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-19 10:42:13 -0700

    added summernote model for content field

--- app_blog/admin.py
+++ app_blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from app_blog.models import *
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,10 +9,11 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 
-class PostAdmin(admin.ModelAdmin):
+class PostAdmin(SummernoteModelAdmin):
     fields = ['title','slug','content' ,'author' ,'category','image' ,'status','published_date']
     list_display = ['title' ,'author' ,'status' ,'published_date']
     list_filter = ['status', 'created_date', 'author', 'category']
     search_fields = ['title', 'author', 'content', 'category']
+    summernote_fields = ['content']
 
 admin.site.register(Post, PostAdmin)
