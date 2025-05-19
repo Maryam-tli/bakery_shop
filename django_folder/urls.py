88bd9a9d3a1a8cd8f86b395b5a501b5709ea8335
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-19 11:01:25 -0700

    added new path for my summernote model

--- django_folder/urls.py
+++ django_folder/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app_bakery.urls')),
     path('blog/', include('app_blog.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
