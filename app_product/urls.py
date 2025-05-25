ef94a6ba9961542d328108002bd0ce30225bdac9
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-25 04:31:22 -0700

    added a new path for the category slug

--- app_product/urls.py
+++ app_product/urls.py
@@ -3,5 +3,6 @@ from app_product.views import *
 
 urlpatterns = [
     path('',home_shop_view, name = 'shop'),
+    path('category/<slug:cat_slug>/', home_shop_view, name = 'category'),
     path('product-detail/<slug:slug>/', detail_shop_view, name = 'detail'),
 ]
