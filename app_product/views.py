d650d24dea74172f865de2e7fb2a35189d7d3e36
Maryam Tavakoli <m.tavakoolii1400@gmail.com>
2025-05-24 19:43:25 -0700

    defined a function for my product details

--- app_product/views.py
+++ app_product/views.py
@@ -16,3 +16,30 @@ def home_shop_view(request):
 
     context={'page_obj' : page_obj}
     return render(request, 'product-list.html', context)
+
+
+def detail_shop_view(request, slug):
+    class_prod = get_object_or_404(Product, slug=slug, status=0, available=True)
+    products = list(Product.objects.filter(status=0, available=True).order_by('-created'))
+    try:
+      index = products.index(class_prod)
+    except ValueError:
+      index = -1
+
+    if index<0:
+        index = 0
+    elif index >= len(products):
+        index = len(products) - 1
+    
+    if index > 0:
+        prev_post = products[index - 1]
+    else:
+        prev_post = None
+
+    if index < len(products) - 1:
+        next_post = products[index + 1]
+    else:
+        next_post = None
+        
+    context = {'class_prod' : class_prod, 'prev_post': prev_post, 'next_post': next_post,}
+    return render(request, 'product-detail.html', context)
